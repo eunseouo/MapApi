4ce176072fc5b214bb64386e85ea3c8e160e7ef1
eunseo <shines3737@naver.com>
2024-07-10 17:54:30 +0900

    new files

---
+++ section3/변수와 자료형.js
@@ -0,0 +1,20 @@
+var num = 1;
+console.log(num);
+
+var num = 10;
+
+// let, const 재선언 불가
+// let:재할당 가능, const: 재할당 불가능
+let num2 = 2;
+num2 = 20;
+
+// const num3 = 3;
+// num3 = 30;
+
+// 문자형 자료 선언
+let string = "안녕"
+
+//논리형 자료 선언
+let bool = true;
+let t = 100 > 10;
+console.log(t);
